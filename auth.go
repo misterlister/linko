9adddb4525a1b87020a56ba7243389486128e0e1
misterlister <haydenglister@gmail.com>
2026-09-08 14:16:58 -0700

    Removed redundant logs

--- auth.go
+++ auth.go
@@ -34,7 +34,7 @@ func (s *server) authMiddleware(next http.Handler) http.Handler {
 		}
 		ok, err := s.validatePassword(password, stored)
 		if err != nil {
-			s.logger.Error("error validating password for user",
+			s.logger.Error("error validating password",
 				slog.String("user", username),
 				slog.String("error", err.Error()),
 			)
@@ -56,9 +56,6 @@ func (s *server) validatePassword(password, stored string) (bool, error) {
 		return false, nil
 	}
 	if err != nil {
-		s.logger.Error("error validating password",
-			slog.String("error", err.Error()),
-		)
 		return false, err
 	}
 	return true, nil
