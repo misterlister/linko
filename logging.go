845436b0aef0e603cf0f29c7abbccb5122af814d
misterlister <haydenglister@gmail.com>
2026-09-10 13:01:19 -0700

    Added stackTracer interface, implemented separate attribute for stack trace info

--- logging.go
+++ logging.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+
+	pkgerr "github.com/pkg/errors"
 )
 
 type closeFunc func() error
+
+type stackTracer interface {
+	error
+	StackTrace() pkgerr.StackTrace
+}
 
 func requestLogger(logger *slog.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
@@ -73,6 +81,15 @@ func replaceAttr(groups []string, a slog.Attr) slog.Attr {
 		err, ok := a.Value.Any().(error)
 		if !ok {
 			return a
+		}
+		if stackErr, ok := errors.AsType[stackTracer](err); ok {
+			return slog.GroupAttrs("error", slog.Attr{
+				Key:   "message",
+				Value: slog.StringValue(stackErr.Error()),
+			}, slog.Attr{
+				Key:   "stack_trace",
+				Value: slog.StringValue(fmt.Sprintf("%+v", stackErr.StackTrace())),
+			})
 		}
 		return slog.String("error", fmt.Sprintf("%+v", err))
 	}
