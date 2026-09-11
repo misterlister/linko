10f67b470461f8ea9ffe8fd9c3425f49dcc3eb49
misterlister <haydenglister@gmail.com>
2026-09-11 15:35:11 -0700

    Added spyResponseWriter

---
+++ spyResponseWriter.go
@@ -0,0 +1,23 @@
+package main
+
+import "net/http"
+
+type spyResponseWriter struct {
+	http.ResponseWriter
+	bytesWritten int
+	statusCode   int
+}
+
+func (w *spyResponseWriter) Write(p []byte) (int, error) {
+	if w.statusCode == 0 {
+		w.statusCode = http.StatusOK
+	}
+	n, err := w.ResponseWriter.Write(p)
+	w.bytesWritten += n
+	return n, err
+}
+
+func (w *spyResponseWriter) WriteHeader(statusCode int) {
+	w.statusCode = statusCode
+	w.ResponseWriter.WriteHeader(statusCode)
+}
