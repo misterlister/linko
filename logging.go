ca90294be19dbd50deb22ee155c0f90d542ccd76
misterlister <haydenglister@gmail.com>
2026-09-11 15:35:59 -0700

    Added read and write details to requestLogger with spy structs

--- logging.go
+++ logging.go
@@ -7,6 +7,7 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"time"
 
 	"boot.dev/linko/internal/linkoerr"
 	pkgerr "github.com/pkg/errors"
@@ -27,11 +28,20 @@ type multiError interface {
 func requestLogger(logger *slog.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			next.ServeHTTP(w, r)
+			spyWriter := &spyResponseWriter{ResponseWriter: w}
+			spyReader := &spyReadCloser{ReadCloser: r.Body}
+			r.Body = spyReader
+			start := time.Now()
+			next.ServeHTTP(spyWriter, r)
+
 			logger.Info("Served request",
 				slog.String("method", r.Method),
 				slog.String("path", r.URL.Path),
 				slog.String("client_ip", r.RemoteAddr),
+				slog.Duration("duration", time.Since(start)),
+				slog.Int("request_body_bytes", spyReader.bytesRead),
+				slog.Int("response_status", spyWriter.statusCode),
+				slog.Int("response_body_bytes", spyWriter.bytesWritten),
 			)
 		})
 	}
