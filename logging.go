6fd2e5cd10b535c1dc6545ad60e5e286ab94c338
misterlister <haydenglister@gmail.com>
2026-09-15 11:59:31 -0700

    Created log context struct, and allowed username to be logged

--- logging.go
+++ logging.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"errors"
 	"fmt"
 	"log/slog"
@@ -25,6 +26,12 @@ type multiError interface {
 	Unwrap() []error
 }
 
+const logContextKey contextKey = "log_context"
+
+type LogContext struct {
+	Username string
+}
+
 func requestLogger(logger *slog.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -32,9 +39,11 @@ func requestLogger(logger *slog.Logger) func(http.Handler) http.Handler {
 			spyReader := &spyReadCloser{ReadCloser: r.Body}
 			r.Body = spyReader
 			start := time.Now()
+			logCtx := &LogContext{Username: ""}
+			r = r.WithContext(context.WithValue(r.Context(), logContextKey, logCtx))
 			next.ServeHTTP(spyWriter, r)
 
-			logger.Info("Served request",
+			logAttrs := []any{
 				slog.String("method", r.Method),
 				slog.String("path", r.URL.Path),
 				slog.String("client_ip", r.RemoteAddr),
@@ -42,7 +51,13 @@ func requestLogger(logger *slog.Logger) func(http.Handler) http.Handler {
 				slog.Int("request_body_bytes", spyReader.bytesRead),
 				slog.Int("response_status", spyWriter.statusCode),
 				slog.Int("response_body_bytes", spyWriter.bytesWritten),
-			)
+			}
+
+			if logCtx.Username != "" {
+				logAttrs = append(logAttrs, slog.String("user", logCtx.Username))
+			}
+
+			logger.Info("Served request", logAttrs...)
 		})
 	}
 }
