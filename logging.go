50419a0861d144a0089d61e42e352954e6b85205
misterlister <haydenglister@gmail.com>
2026-09-17 11:41:48 -0700

    Implemented log rotation

--- logging.go
+++ logging.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"log/slog"
 	"os"
 
 	"github.com/lmittmann/tint"
 	"github.com/mattn/go-isatty"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 type closeFunc func() error
@@ -18,43 +17,35 @@ func initializeLogger(logFile string) (*slog.Logger, closeFunc, error) {
 	disableColour := !(isatty.IsCygwinTerminal(os.Stderr.Fd()) || isatty.IsTerminal(os.Stderr.Fd()))
 
 	if logFile != "" {
-		debugHandler := tint.NewTextHandler(os.Stderr, &tint.Options{
+		handlers := []slog.Handler{}
+
+		handlers = append(handlers, tint.NewTextHandler(os.Stderr, &tint.Options{
 			Level:       slog.LevelDebug,
 			ReplaceAttr: replaceAttr,
 			NoColor:     disableColour,
-		})
+		}))
 
-		file, err := os.OpenFile(logFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0o644)
-		if err != nil {
-			return nil, noOpCloseFunc, fmt.Errorf("failed to open log file: %w", err)
+		logger := &lumberjack.Logger{
+			Filename:   logFile,
+			MaxSize:    1,
+			MaxAge:     28,
+			MaxBackups: 10,
+			LocalTime:  false,
+			Compress:   true,
 		}
-
-		bufferedFile := bufio.NewWriterSize(file, 8192)
+		handlers = append(handlers, slog.NewJSONHandler(logger, &slog.HandlerOptions{
+			ReplaceAttr: replaceAttr,
+		}))
 
 		closeFunc := func() error {
-			err = bufferedFile.Flush()
-			if err != nil {
-				return err
-			}
-			err = file.Close()
-			if err != nil {
-				return err
-			}
-
-			return nil
+			return logger.Close()
 		}
 
-		infoHandler := slog.NewJSONHandler(bufferedFile, &slog.HandlerOptions{
-			Level:       slog.LevelInfo,
-			ReplaceAttr: replaceAttr,
-		})
-
-		logger := slog.New(slog.NewMultiHandler(
-			debugHandler,
-			infoHandler,
+		multiLogger := slog.New(slog.NewMultiHandler(
+			handlers...,
 		))
 
-		return logger, closeFunc, nil
+		return multiLogger, closeFunc, nil
 	}
 
 	return slog.New(tint.NewTextHandler(os.Stderr, &tint.Options{
