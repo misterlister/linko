1ffc30817464aba04c90df28f992d0fbcc680091
misterlister <haydenglister@gmail.com>
2026-09-18 09:35:19 -0700

    Adjusted httpError to obscure sensitive user-side error messages

--- http_error.go
+++ http_error.go
@@ -3,11 +3,20 @@ package main
 import (
 	"context"
 	"net/http"
+	"slices"
 )
 
 func httpError(ctx context.Context, w http.ResponseWriter, status int, err error) {
 	if logCtx, ok := ctx.Value(logContextKey).(*LogContext); ok {
 		logCtx.Error = err
 	}
-	http.Error(w, err.Error(), status)
+
+	errMsg := err.Error()
+
+	sensitiveStatuses := []int{401, 403, 500}
+
+	if slices.Contains(sensitiveStatuses, status) {
+		errMsg = http.StatusText(status)
+	}
+	http.Error(w, errMsg, status)
 }
